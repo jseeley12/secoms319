05bf2c6a6a5a5f375eea0e891e39a9b94a992b81
mlbracht <micbracht8@gmail.com>
2023-10-14 18:24:05 -0500

    testJS

--- midtermProject/index.js
+++ midtermProject/index.js
@@ -2,6 +2,7 @@ fetch("./index.json")
     .then(response => response.json())
     .then(tools => loadMovies(tools));
 
+
 function loadMovies(tools) {
     var Catalog = document.getElementById("toolHolder");
 
@@ -15,17 +16,28 @@ function loadMovies(tools) {
         let toolPlace = document.createElement("div");
         toolPlace.innerHTML = `
         <div class="col">
-            <div class="card shadow-sm">
+            <div class="card shadow-sm" id="cards">
                 <img src="${img}" alt=${name}></img>
-                <div class="card-body">
-                    <p class="card-text">  <h5><Strong>${name}</Strong></h5><br><Strong> Description: </Strong> ${disc}<br><Strong> Price: </Strong> $${price} <br> <a href="${url}" class="btn btn-sm btn-outline-secondary">View</a> </p>
+                <div class="card-body" id="cardtoo">
+                    <p class="card-text"> <h5><Strong>${name}</Strong></h5><br></p>
                 </div>
             </div>
         </div>`;
-
         Catalog.appendChild(toolPlace);
+
+        let toolPlace2 = document.createElement("div");
+        toolPlace2.innerHTML = `
+        <div id="smaller"><p><Strong> Description: </Strong> ${disc}<br><Strong> Price: </Strong> $${price} <br> <a href="${url}" class="btn btn-sm btn-outline-secondary">View</a> </p></div>
+        `;
+        
+        toolPlace.childNodes[1].childNodes[1].childNodes[3].appendChild(toolPlace2);
+
+        // var toggleButton3 = document.getElementById('cards');
+        // var card3 = document.getElementById('smaller');
+        var collapsableCard3 = new bootstrap.Collapse(toolPlace2, { toggle: true });
+        toolPlace.addEventListener('click', function () {
+            collapsableCard3.toggle();
+            console.log("hi!")
+        });
     }
-
-
-
 }
