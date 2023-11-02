f78138b86dabb282acbb81a4a3519832d6e98dc2
jseeley12 <joshseeley1821@gmail.com>
2023-11-02 17:14:01 -0500

    s

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>HELLLLLLLOOOOOO</code> and save to reload.
         </p>
         <a
           className="App-link"
