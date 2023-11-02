cadd71c2336d36f4e67194af8b9731ba4b2b330d
jseeley12 <joshseeley1821@gmail.com>
2023-11-02 16:11:11 -0500

    f

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import data from './app.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
