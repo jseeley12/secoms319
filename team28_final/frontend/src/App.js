aba6cfc7c24a6eae808e0b501a9f404c3b330384
mlbracht <micbracht8@gmail.com>
2023-11-26 22:51:29 -0600

    ordering

--- team28_final/frontend/src/App.js
+++ team28_final/frontend/src/App.js
@@ -141,6 +141,7 @@ function App() {
   const [userInfo, setUserInfo] = useState(false);
   const [confirmation, setconfirmation] = useState(false);
   const [error, setError] = useState(false);
+  const [help, setHelp] = useState(false);
 
   function ShowCatalog() {
     setQuery("");
@@ -150,6 +151,7 @@ function App() {
     setUserInfo(false);
     setconfirmation(false);
     setError(false);
+    setHelp(false);
   }
 
   function ShowCheckout() {
@@ -158,7 +160,128 @@ function App() {
     setUserInfo(true);
     setconfirmation(false);
     setError(false);
+    setHelp(false);
   }
+
+  function ShowConfirmation() {
+    setcheckout(true);
+    setcatalog(false);
+    setUserInfo(false);
+    setconfirmation(true);
+    setError(false);
+    setHelp(false);
+  }
+
+  function ShowError() {
+    setcheckout(true);
+    setcatalog(false);
+    setUserInfo(true);
+    setconfirmation(false);
+    setError(true);
+    setHelp(false);
+  }
+
+  function ShowCatalog2() {
+    window.location.reload();
+  }
+
+  function ShowHelp(){
+    setcheckout(false);
+    setcatalog(false);
+    setUserInfo(false);
+    setconfirmation(false);
+    setError(false);
+    setHelp(true);
+  }
+
+  //Ordering Form
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cardNum, setcardNum] = useState("");
+  const [adress, setAdress] = useState("");
+  const [town, setTown] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [errorState, seterrorState] = useState("");
+
+  const nameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const emailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const CardChange = (e) => {
+    setcardNum(e.target.value);
+  };
+
+  const AdressChange = (e) => {
+    setAdress(e.target.value);
+  };
+
+  const townChange = (e) => {
+    setTown(e.target.value);
+  };
+
+  const stateChange = (e) => {
+    setState(e.target.value);
+  };
+
+  const zipChange = (e) => {
+    setZip(e.target.value);
+  };
+
+  //testing form
+  function ConfirmationInfo() {
+    let val = true;
+
+    if (
+      !email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      val = false;
+      seterrorState("Email is not valid");
+    }
+
+    if (name.length == 0) {
+      val = false;
+      seterrorState("Please add your Name");
+    }
+
+    if (!(cardNum.match(/^[0-9]{4}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}$/) && (cardNum.length == 16||cardNum.length == 19))) {
+      val = false;
+      seterrorState("Card Number is not valid");
+    }
+
+    if (adress.length == 0) {
+      val = false;
+      seterrorState("Please add your adress");
+    }
+
+    if (town.length == 0) {
+      val = false;
+      seterrorState("Please add your City");
+    }
+
+    if (state.length == 0){
+      val = false
+      seterrorState("Please add your State");
+    }
+
+    if (!zip.match(/^[0-9]{5}$/)){
+      val = false
+      seterrorState("Zip Code is not valid");
+    }
+  
+    if(val){
+      ShowConfirmation()
+    }else{
+      ShowError()
+    }
+  }
+
 
   return (
     <>
@@ -182,13 +305,13 @@ function App() {
             <hr></hr>
             <h4>Search</h4>
             <input
-              className="searchbarbtn"
+              className="form-control"
               type="search"
               value={query}
               onChange={handleChange}
             />
             <hr></hr>
-            <botton className="btn btn-primary my-2">Help</botton>
+            <botton className="btn btn-primary my-2" onClick={ShowHelp}>Help</botton>
           </div>
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div className="container">
@@ -200,13 +323,107 @@ function App() {
         </div>
       </div>}
 
+      {/* Checkout Page */}
       {checkOut && <div className="container-fluid">
         <div className="row">
           <div className="sidebar border border-left col-md-3 col-lg-2 p-0 bg-body-tertiary">
             <h3>Cart ({cart.length})</h3>
-            <botton className="btn btn-primary my-2" onClick={ShowCatalog}>Return to Catalog</botton>
+            {userInfo && <botton className="btn btn-primary my-2" onClick={ShowCatalog}>Return to Catalog</botton>}
+            {confirmation && <botton className="btn btn-primary my-2" onClick={ShowCatalog2}>Return to Catalog</botton>}
             <hr></hr>
             <h4>Price: ${cartTotal}</h4>
+            <hr></hr>
+            {/* User Info */}
+            {userInfo && <div>
+              <h5>User Info:</h5>
+              {error && <div style={{ backgroundColor: "red"}}>
+                  <h3>
+                    <i class="bi-exclamation-circle"></i> Something went wrong!
+                  </h3>
+                  {errorState}
+                </div>}
+              <label for="inputName" class="form-label">
+                Full Name
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                onChange={nameChange}
+              />
+              <label for="inputEmail4" class="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                onChange={emailChange}
+              />
+              <label for="inputCard" class="form-label">
+                Card
+              </label>
+              <input
+                type="text"
+                id="inputCard"
+                class="form-control"
+                placeholder="XXXX-XXXX-XXXX-XXXX"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={CardChange}
+              />
+              <label for="inputAddress" class="form-label">
+                Address
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputAddress"
+                placeholder="1234 Main St"
+                onChange={AdressChange}
+              />
+              <label for="inputCity" class="form-label">
+                City
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                onChange={townChange}
+              />
+              <label for="inputState" class="form-label">
+                State
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                onChange={stateChange}
+              />
+              <label for="inputZip" class="form-label">
+                Zip
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                onChange={zipChange}
+              />
+              <button class="btn btn-success" onClick={ConfirmationInfo}>
+                <i class="bi-bag-check"></i> Order
+              </button>
+  
+            </div>}
+
+            {/* Confirmation Page */}
+            {confirmation && 
+            <div>
+              <h5>✅ Items Purchased ✅</h5>
+              <h5>Name: {name}</h5>
+              <h5>Email: {email}</h5>
+              <h5>Card: {cardNum}</h5>
+              <h5>Name: {name}</h5>
+              <h5>Adress: {adress}</h5>
+              <h5>State: {state}</h5>
+              <h5>ZIP: {zip}</h5>
+            </div>
+            }
+
           </div>
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div className="container">
@@ -217,6 +434,21 @@ function App() {
           </main>
         </div>
       </div>}
+
+      {/* Sign in Sheet */}
+
+
+
+
+      {/* Help Page */}
+      {help && <div>
+        <botton className="btn btn-primary my-2" onClick={ShowCatalog}>Return to Catalog</botton>
+        
+        
+        
+      </div>}
+
+
       <footer>
         <h1>Footer</h1>
       </footer>
