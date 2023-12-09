a0fdd1d0c63c48e2cf27d1da6f55df3686947a05
mlbracht <micbracht8@gmail.com>
2023-12-08 20:27:08 -0600

    updates

--- assignment3/backend/App.js
+++ assignment3/backend/App.js
@@ -16,53 +16,68 @@ const db = client.db(dbName);
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
 const port = "8081";
 const host = "localhost";
 
-//Mongo
-app.get("/listTools", async (req, res) => {
-    await client.connect();
-    console.log("Node connected successfully to GET MongoDB");
-    const query = {};
-    const results = await db
-        .collection("tools")
-        .find(query)
-        .limit(100)
-        .toArray();
-    console.log(results);
-    res.status(200);
-    res.send(results);
+//Mongo Get
+app.get("/listItems", async (req, res) => {
+  await client.connect();
+  console.log("Node connected successfully to GET MongoDB");
+  const query = {};
+  const results = await db.collection("tools").find(query).limit(100).toArray();
+  console.log(results);
+  res.status(200);
+  res.send(results);
 });
 
-app.post("/addTool", async (req, res) => {
-    await client.connect();
-    const keys = Object.keys(req.body);
-    const values = Object.values(req.body);
-    const id = values[0]; // id
-    const name = values[1]; // name
-    const price = values[2]; // price
-    const description = values[3]; // description
-    const imageUrl = values[4]; // imageUrl
-    console.log(id, name, price, description, imageUrl);
-    const newDocument = {
-       //form for new tool
-    };
-    const results = await db.collection("tools").insertOne(newDocument);
-    res.status(200);
-    res.send(results);
-  });
-  
-  app.delete("/deleteTool", async (req, res) => {
-    await client.connect();
-    // const keys = Object.keys(req.body);
-    const values = Object.values(req.body);
-    const id = values[0]; // id
-    console.log("Tool to delete :",id);
-    const query = { id: id };
-    const results = await db.collection("tools").deleteOne(query);
-    res.status(200);
-    res.send(results);
-    });
+//Mongo Get ID
+app.get("/:id", async (req, res) => {
+  const robotid = Number(req.params.id);
+  console.log("Robot to find :", robotid);
+  await client.connect();
+  console.log("Node connected successfully to GET-id MongoDB");
+  const query = { id: robotid };
+  const results = await db.collection("robots").findOne(query);
+  console.log("Results :", results);
+  if (!results) res.send("Not Found").status(404);
+  else res.send(results).status(200);
+});
+
+//Mongo Add
+app.post("/addItems", async (req, res) => {
+  await client.connect();
+  const keys = Object.keys(req.body);
+  const values = Object.values(req.body);
+  const id = values[0]; // id
+  const name = values[1]; // name
+  const price = values[2]; // price
+  const description = values[3]; // description
+  const imageUrl = values[4]; // imageUrl
+  console.log(id, name, price, description, imageUrl);
+  const newDocument = {
+    //form for new tool
+  };
+  const results = await db.collection("tools").insertOne(newDocument);
+  res.status(200);
+  res.send(results);
+});
+
+//Mongo Delete
+app.delete("/deleteItems", async (req, res) => {
+  await client.connect();
+  // const keys = Object.keys(req.body);
+  const values = Object.values(req.body);
+  const id = values[0]; // id
+  console.log("Item to delete :", id);
+  const query = { id: id };
+  const results = await db.collection("tools").deleteOne(query);
+  res.status(200);
+  res.send(results);
+});
+
 app.listen(port, () => {
-    console.log("App listening at http://%s:%s", host, port);
-  });
+  console.log("App listening at http://%s:%s", host, port);
+});
