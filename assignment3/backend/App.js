54d8af10e9bc83ee28fd8a251f446349662e1095
mlbracht <micbracht8@gmail.com>
2023-12-08 22:55:30 -0600

    Seach bar

--- assignment3/backend/App.js
+++ assignment3/backend/App.js
@@ -9,7 +9,7 @@ console.log("test");
 
 //Mongo
 const url = "mongodb://127.0.0.1:27017";
-const dbName = "secoms319";
+const dbName = "reactdata";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
@@ -27,7 +27,7 @@ app.get("/listItems", async (req, res) => {
   await client.connect();
   console.log("Node connected successfully to GET MongoDB");
   const query = {};
-  const results = await db.collection("tools").find(query).limit(100).toArray();
+  const results = await db.collection("fakestore_catalog").find(query).limit(100).toArray();
   console.log(results);
   res.status(200);
   res.send(results);
@@ -40,7 +40,7 @@ app.get("/:id", async (req, res) => {
   await client.connect();
   console.log("Node connected successfully to GET-id MongoDB");
   const query = { id: robotid };
-  const results = await db.collection("robots").findOne(query);
+  const results = await db.collection("fakestore_catalog").findOne(query);
   console.log("Results :", results);
   if (!results) res.send("Not Found").status(404);
   else res.send(results).status(200);
@@ -60,7 +60,7 @@ app.post("/addItems", async (req, res) => {
   const newDocument = {
     //form for new tool
   };
-  const results = await db.collection("tools").insertOne(newDocument);
+  const results = await db.collection("fakestore_catalog").insertOne(newDocument);
   res.status(200);
   res.send(results);
 });
@@ -73,7 +73,7 @@ app.delete("/deleteItems", async (req, res) => {
   const id = values[0]; // id
   console.log("Item to delete :", id);
   const query = { id: id };
-  const results = await db.collection("tools").deleteOne(query);
+  const results = await db.collection("fakestore_catalog").deleteOne(query);
   res.status(200);
   res.send(results);
 });
--- assignment3/frontend/src/App.js
+++ assignment3/frontend/src/App.js
@@ -5,8 +5,14 @@ import "./App.css";
 function App() {
   //Category
   const [ProductsCategory, setProductsCategory] = useState([]);
+  const [oneProduct, setOneProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  function getAllProducts() {
     setIsLoading(true);
     fetch("http://localhost:8081/listItems")
       .then((response) => response.json())
@@ -15,55 +21,69 @@ function App() {
         setProductsCategory(res);
         setIsLoading(false);
       });
-  }, []);
+  }
+
+  function getOneProducts(id) {
+    setIsLoading(true);
+    fetch("http://localhost:8081/"+id)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        setOneProduct(res);
+        setIsLoading(false);
+      });
+  }
 
   const listItems = ProductsCategory.map((el) => (
     <div key={el.id} className="col">
       <div style={{ backgroundColor: "#ECECEC" }} className="card shadow-sm">
-        <img className="imgnew" src={el.img} width={200} /> <br />
+        <img className="imgnew" src={el.image} width={200} /> <br />
         <div className="card-body" id="cardtoo">
           <p className="title">
-            {el.name} <br />
+            {el.title} <br />
           </p>
-          {el.discription} <br />
+          {el.description} <br />
           <br />
           <span className="price">
-            <span className="commentPrice">{"For the small price of:"}</span>{" "}
-            {"    "}
-            {"$"}
+            Price:
+            {" $"}
             {el.price}
-            <br /> <br />
+            <br />
           </span>
-          <div style={{ textAlign: "center" }}>
-            <button
-              type="button"
-              variant="light"
-              onClick={() => removeFromCart(el)}
-            >
-              -
-            </button>{" "}
-            {el.qty}{" "}
-            <button type="button" variant="light" onClick={() => addToCart(el)}>
-              {" "}
-              +{" "}
-            </button>
-          </div>
+          Category: {el.category} <br />
+          Rating: {el.rating}
+          <br />
+          Id: {el.id}
         </div>
       </div>
     </div>
   ));
+
+  //ID Find
+  const [queryID, setQueryID] = useState("");
+
+  const FindQueryID = (e) => {
+    setQueryID(e.target.value);
+  };
+
+  function SelectButton() {
+    getOneProducts(queryID);
+    console.log(queryID);
+  };
 
   //Displays
   const [ListProduct, setListProduct] = useState(true);
   const [UpdateProduct, setUpdateProduct] = useState(false);
   const [AddProduct, setAddProduct] = useState(false);
   const [DeleteProduct, setDeleteProduct] = useState(false);
+  const [Info, setInfo] = useState(false);
 
   function ShowList() {
     setListProduct(true);
     setUpdateProduct(false);
     setAddProduct(false);
     setDeleteProduct(false);
+    setInfo(false);
   }
 
   function ShowUpdate() {
@@ -71,6 +91,7 @@ function App() {
     setUpdateProduct(true);
     setAddProduct(false);
     setDeleteProduct(false);
+    setInfo(false);
   }
 
   function ShowAdd() {
@@ -78,6 +99,7 @@ function App() {
     setUpdateProduct(false);
     setAddProduct(true);
     setDeleteProduct(false);
+    setInfo(false);
   }
 
   function ShowDelete() {
@@ -85,36 +107,118 @@ function App() {
     setUpdateProduct(false);
     setAddProduct(false);
     setDeleteProduct(true);
+    setInfo(false);
+  }
+
+  function ShowInfo() {
+    setListProduct(false);
+    setUpdateProduct(false);
+    setAddProduct(false);
+    setDeleteProduct(false);
+    setInfo(true);
+  }
+
+  //add -  Mongo
+  function postMethod() {
+    fetch("http://localhost:8081/addRobot", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        id: 4,
+        name: "Robot 4",
+        price: 124.5,
+        description: "This is a description of Robot 4",
+        imageUrl: "https://robohash.org/",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        var container = document.getElementById("showData");
+        container.innerHTML = JSON.stringify(data);
+      });
+  }
+
+  //delete -  Mongo
+  function deleteMethod(id) {
+    fetch("http://localhost:8081/deleteItem", {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        id: id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        var container = document.getElementById("showData");
+        container.innerHTML = JSON.stringify(data);
+      })
+      .catch((err) => console.log("Errror:" + err));
   }
 
   return (
     <>
       <header className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <div className="container-fluid d-flex align-items-center">
-          Assignment 3
-          
+          <h2 className="d-flex align-items-center fs-4 text-white mb-0">
+            Assignment 3
+          </h2>
+          <br />
+          <div>
+            <botton className="btn btn-primary my-2" onClick={ShowInfo}>
+              Info
+            </botton>
+            .
+            <botton className="btn btn-primary my-2" onClick={ShowList}>
+              Read
+            </botton>
+            .
+            <botton className="btn btn-primary my-2" onClick={ShowAdd}>
+              Create
+            </botton>
+            .
+            <botton className="btn btn-primary my-2" onClick={ShowUpdate}>
+              Update
+            </botton>
+            .
+            <botton className="btn btn-primary my-2" onClick={ShowDelete}>
+              Delete
+            </botton>
+          </div>
         </div>
       </header>
 
       {/* Get */}
-      {ListProduct && <div>
+      {ListProduct && (
+        <div>
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 ">
+              {listItems}
+            </div>
+          </div>
+        </div>
+      )}
 
-      </div>}
-      
       {/* Add */}
-      {AddProduct && <div>
-
-      </div>}
+      {AddProduct && <div></div>}
 
       {/* Update */}
-      {UpdateProduct && <div>
-
-      </div>}
+      {UpdateProduct && <div></div>}
 
       {/* Delete */}
-      {DeleteProduct && <div>
-        
-      </div>}
+      {DeleteProduct && (
+        <div>
+          <h4>ID to delete</h4>
+          <input 
+            type="search" 
+            value={queryID}
+            onChange={FindQueryID}/>
+            <botton className="btn btn-primary my-2" onClick={SelectButton}>
+              Seach
+            </botton>
+        </div>
+      )}
 
       <footer
         class="text-body-secondary py-5 background"
@@ -124,9 +228,7 @@ function App() {
           <p class="float-end mb-2">
             <a href="#">Back to top</a>
           </p>
-          <p class="mb-6">
-            COM S 319 - Assignment 3
-          </p>
+          <p class="mb-6">COM S 319 - Assignment 3</p>
         </div>
       </footer>
     </>
